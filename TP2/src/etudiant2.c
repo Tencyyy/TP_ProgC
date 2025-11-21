0832d789c6246a5ffe69f758188564543e9196cb
Stephen <Tency_160@pm.me>
2025-11-21 15:29:04 +0100

    Update etudiant2.c

--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -38,52 +38,6 @@ int main() {
     strcpy(etudiants[3].nom, "Lambert");
     strcpy(etudiants[3].prenom, "Lucas");
     strcpy(etudiants[3].adresse, "8, Avenue du Pra (modifié)vendredi 21 novembre 2025 15:27
-:100:
-Clique pour réagir
-:joy:
-Clique pour réagir
-:thumbsup:
-Clique pour réagir
-Ajouter une réaction
-Répondre
-Transférer
-Plus
-[15:23]vendredi 21 novembre 2025 15:23
-couleurs.c #include <stdio.h>
 
-#define NB_COULEURS 10
-
-// Définition d'une structure pour représenter une couleur RGBA
-struct Couleur {
-    unsigned char r; // Rouge
-    unsigned char g; // Vert
-    unsigned char b; // Bleu
-    unsigned char a; // Alpha
-};
-
-int main() {
-    struct Couleur couleurs[NB_COULEURS] = {
-        {0xef, 0x78, 0x12, 0xff},
-        {0x2c, 0xc8, 0x64, 0xff},
-        {0x00, 0x00, 0x00, 0xff},
-        {0xff, 0xff, 0xff, 0xff},
-        {0x12, 0x34, 0x56, 0x99},
-        {0xab, 0xcd, 0xef, 0xdd},
-        {0x10, 0x20, 0x30, 0x40},
-        {0x80, 0x00, 0x80, 0xff},
-        {0xfa, 0x80, 0x72, 0xff},
-        {0x33, 0x66, 0x99, 0xaa}
-    };
-
-    printf("===== Liste des couleurs (RGBA) =====\n\n");
-
-    for (int i = 0; i < NB_COULEURS; i++) {
-        printf("Couleur %d :\n", i + 1);
-        printf("Rouge : %u\n", couleurs[i].r);
-        printf("Vert  : %u\n", couleurs[i].g);
-        printf("Bleu  : %u\n", couleurs[i].b);
-        printf("Alpha : %u\n\n", couleurs[i].a);
-    }
-
-    return 0;
 } 
+
