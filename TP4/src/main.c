e1c8d6cf9c8cc75948fc0b502117ed6b2eafa5ec
Stephen <Tency_160@pm.me>
2025-12-01 10:17:32 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,29 +1,69 @@
-
 #include <stdio.h>
 #include "operator.h"
+#include "fichier.h"
+#include "liste.h"
+
+void run_41() {
+    int a, b;
+    char op;
+    printf("num1 : "); scanf("%d", &a);
+    printf("num2 : "); scanf("%d", &b);
+    printf("Opérateur : "); scanf(" %c", &op);
+
+    int result = 0;
+    switch (op) {
+        case '+': result = addition(a, b); break;
+        case '-': result = soustraction(a, b); break;
+        case '*': result = produit(a, b); break;
+        case '/': result = quotient(a, b); break;
+        case '%': result = modulo(a, b); break;
+        case '&': result = et_logique(a, b); break;
+        case '|': result = ou_logique(a, b); break;
+        case '~': result = negation(a); break;
+    }
+    printf("Résultat : %d\n", result);
+}
+
+void run_42() {
+    int choix;
+    char nom[50], msg[200];
+
+    printf("1. Lire un fichier\n2. Écrire un fichier\nVotre choix : ");
+    scanf("%d", &choix);
+
+    printf("Nom du fichier : ");
+    scanf("%s", nom);
+
+    if (choix == 1) lire_fichier(nom);
+    else {
+        printf("Message : ");
+        scanf(" %[^\n]", msg);
+        ecrire_dans_fichier(nom, msg);
+    }
+}
+
+void run_47() {
+    struct liste_couleurs l;
+    init_liste(&l);
+
+    for (int i = 0; i < 10; i++) {
+        struct couleur c = { i*20, i*10, i*5, 255 };
+        insertion(&c, &l);
+    }
+
+    printf("Liste :\n");
+    parcours(&l);
+}
 
 int main() {
-    int num1, num2;
-    char op;
+    int choix;
+    printf("Choisir exercice (41, 42, 47) : ");
+    scanf("%d", &choix);
 
-    printf("Entrez num1 : ");
-    scanf("%d", &num1);
-    printf("Entrez num2 : ");
-    scanf("%d", &num2);
-    printf("Entrez l'opérateur (+, -, *, /, %%, &, |, ~) : ");
-    scanf(" %c", &op);
-
-    switch(op) {
-        case '+': printf("Résultat : %d\n", somme(num1, num2)); break;
-        case '-': printf("Résultat : %d\n", difference(num1, num2)); break;
-        case '*': printf("Résultat : %d\n", produit(num1, num2)); break;
-        case '/': printf("Résultat : %d\n", quotient(num1, num2)); break;
-        case '%': printf("Résultat : %d\n", modulo(num1, num2)); break;
-        case '&': printf("Résultat : %d\n", et(num1, num2)); break;
-        case '|': printf("Résultat : %d\n", ou(num1, num2)); break;
-        case '~': printf("Résultat : %d\n", negation(num1)); break;
-        default: printf("Opérateur invalide.\n");
-    }
+    if (choix == 41) run_41();
+    else if (choix == 42) run_42();
+    else if (choix == 47) run_47();
+    else printf("Invalide.\n");
 
     return 0;
 }
